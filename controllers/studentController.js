91ddb7eb509ee19b9b46ed148194a4ff4036fd32
XndrYG <xygarcia6406@gmail.com>
2022-01-28 09:17:44 -0800

    finished with part 2

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -13,11 +13,51 @@ module.exports.viewProfile = async function(req, res){
 }
 
 //render add
+module.exports.renderAddForm = function(req, res){
+    const student = {
+        first_name: '',
+        last_name: '',
+        grade_level: 9,
+    }
+    res.render('student/add', {student});
+}
 
 //add
+module.exports.addStudent = async function(req, res){
+    const student = await Student.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        grade_level: req.body.grade_level
+    });
+    res.redirect(`/students/profile/${student.id}`);
+}
 
 //render edit
+module.exports.renderEditForm = async function(req, res){
+    const student = await Student.findByPk(req.params.id);
+    res.render('student/edit', {student});
+}
 
-//edit
+//update
+module.exports.updateStudent = async function(req, res){
+    const student = await Student.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        grade_level: req.body.grade_level,
+    },  {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/students/profile/${req.params.id}`);
+}
 
 //delete
+module.exports.deleteStudent = async function(req, res){
+    await Student.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect('/students');
+}
